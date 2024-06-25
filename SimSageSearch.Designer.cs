64161a40e73860da18c79fbe3a7f41e1a9895e3b
Rock de Vocht <rock@simsage.co.uk>
2024-06-25 14:35:23 +0100

    initial release

---
+++ SimSageSearch.Designer.cs
@@ -0,0 +1,164 @@
+﻿using SimSageSearch.Properties;
+
+namespace SimSageSearch
+{
+    partial class SimSageSearch
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            components = new System.ComponentModel.Container();
+            tbQueryString = new TextBox();
+            pbButton = new Button();
+            bindingSource1 = new BindingSource(components);
+            pictureBoxClose = new PictureBox();
+            pictureBoxSettings = new PictureBox();
+            contextMenuSettings = new ContextMenuStrip(components);
+            toolStripTextURL = new ToolStripTextBox();
+            toolStripSeparator1 = new ToolStripSeparator();
+            toolStripMenuExit = new ToolStripMenuItem();
+            pictureBox1 = new PictureBox();
+            ((System.ComponentModel.ISupportInitialize)bindingSource1).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBoxClose).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBoxSettings).BeginInit();
+            contextMenuSettings.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            SuspendLayout();
+            // 
+            // tbQueryString
+            // 
+            tbQueryString.BorderStyle = BorderStyle.FixedSingle;
+            tbQueryString.Location = new Point(12, 35);
+            tbQueryString.Name = "tbQueryString";
+            tbQueryString.Size = new Size(232, 23);
+            tbQueryString.TabIndex = 0;
+            // 
+            // pbButton
+            // 
+            pbButton.DialogResult = DialogResult.OK;
+            pbButton.Location = new Point(255, 35);
+            pbButton.Name = "pbButton";
+            pbButton.Size = new Size(75, 23);
+            pbButton.TabIndex = 1;
+            pbButton.Text = "Search";
+            pbButton.UseVisualStyleBackColor = true;
+            pbButton.Click += pbButton_Click;
+            // 
+            // pictureBoxClose
+            // 
+            pictureBoxClose.Image = Resources.Close;
+            pictureBoxClose.Location = new Point(323, 3);
+            pictureBoxClose.Name = "pictureBoxClose";
+            pictureBoxClose.Size = new Size(16, 16);
+            pictureBoxClose.TabIndex = 2;
+            pictureBoxClose.TabStop = false;
+            pictureBoxClose.Click += pictureBoxClose_Click;
+            // 
+            // pictureBoxSettings
+            // 
+            pictureBoxSettings.Image = Resources.Settings;
+            pictureBoxSettings.Location = new Point(301, 3);
+            pictureBoxSettings.Name = "pictureBoxSettings";
+            pictureBoxSettings.Size = new Size(16, 16);
+            pictureBoxSettings.TabIndex = 3;
+            pictureBoxSettings.TabStop = false;
+            pictureBoxSettings.Click += pictureBoxSettings_Click;
+            // 
+            // contextMenuSettings
+            // 
+            contextMenuSettings.Items.AddRange(new ToolStripItem[] { toolStripTextURL, toolStripSeparator1, toolStripMenuExit });
+            contextMenuSettings.Name = "contextMenuSettings";
+            contextMenuSettings.Size = new Size(261, 79);
+            contextMenuSettings.Closing += contextMenuSettings_Closing;
+            contextMenuSettings.Opening += contextMenuSettings_Opening;
+            // 
+            // toolStripTextURL
+            // 
+            toolStripTextURL.Name = "toolStripTextURL";
+            toolStripTextURL.Size = new Size(200, 23);
+            toolStripTextURL.Text = "demo.simsage.ai";
+            // 
+            // toolStripSeparator1
+            // 
+            toolStripSeparator1.Name = "toolStripSeparator1";
+            toolStripSeparator1.Size = new Size(257, 6);
+            // 
+            // toolStripMenuExit
+            // 
+            toolStripMenuExit.Name = "toolStripMenuExit";
+            toolStripMenuExit.Size = new Size(260, 22);
+            toolStripMenuExit.Text = "&Exit";
+            toolStripMenuExit.Click += toolStripMenuExit_Click;
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.Image = Resources.Logo;
+            pictureBox1.Location = new Point(14, 4);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(100, 25);
+            pictureBox1.TabIndex = 4;
+            pictureBox1.TabStop = false;
+            // 
+            // SimSageSearch
+            // 
+            AcceptButton = pbButton;
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(342, 67);
+            Controls.Add(pictureBox1);
+            Controls.Add(pictureBoxSettings);
+            Controls.Add(pictureBoxClose);
+            Controls.Add(pbButton);
+            Controls.Add(tbQueryString);
+            FormBorderStyle = FormBorderStyle.None;
+            Name = "SimSageSearch";
+            Text = "Form1";
+            Deactivate += Form1_Deactivate;
+            Leave += Form1_Leave;
+            ((System.ComponentModel.ISupportInitialize)bindingSource1).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBoxClose).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBoxSettings).EndInit();
+            contextMenuSettings.ResumeLayout(false);
+            contextMenuSettings.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private TextBox tbQueryString;
+        private Button pbButton;
+        private BindingSource bindingSource1;
+        private PictureBox pictureBoxClose;
+        private PictureBox pictureBoxSettings;
+        private ContextMenuStrip contextMenuSettings;
+        private ToolStripTextBox toolStripTextURL;
+        private PictureBox pictureBox1;
+        private ToolStripSeparator toolStripSeparator1;
+        private ToolStripMenuItem toolStripMenuExit;
+    }
+}
---
+++ SimSageSearch.cs
@@ -0,0 +1,112 @@
+using System.Diagnostics;
+using Microsoft.Win32;
+
+namespace SimSageSearch
+{
+    public partial class SimSageSearch : Form
+    {
+        private readonly string strKeyPath = @"HKEY_CURRENT_USER\SimSageSearch";
+        private readonly string strKeyName = "URL";
+
+        private bool blSubWindowActive = false;
+        public SimSageSearch()
+        {
+            InitializeComponent();
+
+            WindowState = FormWindowState.Minimized;
+            SetWindowPosition();
+        }
+
+        private void SetWindowPosition()
+        {
+            // Set the position of the SimSage search window - this could be dynamic
+            int y = Screen.FromPoint(MousePosition).Bounds.Bottom - (Height * 2);
+            int x = (Screen.FromPoint(MousePosition).Bounds.Right - Width) / 2;
+
+            Location = new Point(x, y);
+        }
+
+        #region Button Handlers
+        private void pbButton_Click(object sender, EventArgs e)
+        {
+            if (tbQueryString.TextLength == 0) return;
+
+            string strSimSageURL = GetRegistryEntryURL();
+
+            try
+            {
+                Process.Start(new ProcessStartInfo
+                {
+                    FileName = $"https://{strSimSageURL}/search?query={tbQueryString.Text}",
+                    UseShellExecute = true
+                });
+
+                WindowState = FormWindowState.Minimized;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(@$"Error opening the browser: {ex.Message}", "Error", MessageBoxButtons.OK);
+            }
+        }
+
+        private void pictureBoxSettings_Click(object sender, EventArgs e)
+        {
+            contextMenuSettings.Show(MousePosition, ToolStripDropDownDirection.Default);
+        }
+
+        private void pictureBoxClose_Click(object sender, EventArgs e)
+        {
+            // This really means minimize rather than close
+            this.WindowState = FormWindowState.Minimized;
+        }
+        #endregion
+
+        private void Form1_Leave(object sender, EventArgs e)
+        {
+            this.WindowState = FormWindowState.Minimized;
+        }
+
+        private void Form1_Deactivate(object sender, EventArgs e)
+        {
+            // If the messagebox is active just leave
+            if (blSubWindowActive) return;
+
+            this.WindowState = FormWindowState.Minimized;
+        }
+
+        #region Context Menu Event Handling
+        private void contextMenuSettings_Closing(object sender, EventArgs e)
+        {
+            SetRegistryEntry(toolStripTextURL.Text);
+            blSubWindowActive = false;
+        }
+
+        private void contextMenuSettings_Opening(object sender, EventArgs e)
+        {
+            toolStripTextURL.Text = GetRegistryEntryURL();
+            blSubWindowActive = true;
+        }
+        private void toolStripMenuExit_Click(object sender, EventArgs e)
+        {
+            blSubWindowActive = true;
+            if (MessageBox.Show("Are you sure you want to close SimSage Search?", "Are you sure?", MessageBoxButtons.YesNo, MessageBoxIcon.Question) == DialogResult.Yes) Close();
+            blSubWindowActive = false;
+        }
+        #endregion
+
+        #region Registry Setting and Getting
+        private void SetRegistryEntry(string strURL)
+        {
+            Registry.SetValue(strKeyPath, strKeyName, strURL);
+        }
+
+        private string GetRegistryEntryURL()
+        {
+            string? strResult = Registry.GetValue(strKeyPath, strKeyName, "demo.simsage.ai")?.ToString() ?? "demo.simsage.ai";
+
+            return (strResult);
+        }
+        #endregion
+
+    }
+}
